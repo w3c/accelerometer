fc1799aadca0d5f7b1374b6e12f6f219f5ec15df
Fuqiao Xue <xfq@w3.org>
2019-09-29 14:42:45 +0800

    Copy the images

--- compile.sh
+++ compile.sh
@@ -4,6 +4,7 @@ set -e # Exit with nonzero exit code if anything fails
 bikeshed spec
 
 if [ -d out ]; then
-  echo Copy the generated spec into out/index.html
+  echo Copy the generated spec into out/
   cp index.html out/index.html
+  cp -R images/ out/images/
 fi
